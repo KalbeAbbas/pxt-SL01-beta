60fa6e912fba62f3dec325d515acb77fcd588169
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-05 16:40:35 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,37 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+enum SI1145_ADDR
+{
+    //% block="0x60"
+    ADDR_0x60 = 0x60
+}
+
+namespace SL01
+{
+    let SI1145_I2C_ADDR = SI1145_ADDR.ADDR_0x60
+
+    function setreg(reg: number, dat: number): void {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = dat;
+        pins.i2cWriteBuffer(SI1145_I2C_ADDR, buf);
+    }
+
+    function getreg(reg: number): number {
+        pins.i2cWriteNumber(SI1145_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(SI1145_I2C_ADDR, NumberFormat.UInt8BE);
+    }
+
+    function getInt8LE(reg: number): number {
+        pins.i2cWriteNumber(SI1145_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(SI1145_I2C_ADDR, NumberFormat.Int8LE);
+    }
+
+    function getUInt16LE(reg: number): number {
+        pins.i2cWriteNumber(SI1145_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(SI1145_I2C_ADDR, NumberFormat.UInt16LE);
+    }
+
+    function getInt16LE(reg: number): number {
+        pins.i2cWriteNumber(SI1145_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(SI1145_I2C_ADDR, NumberFormat.Int16LE);
+    }
+} 
