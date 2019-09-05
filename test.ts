1c315dfa23e63813b0bf4f542704343bcbc8dcd3
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-05 19:24:07 +0500

    Updates.

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
-// tests go here; this will not be compiled when this package is used as a library
+let res = SL01.Begin()
+basic.pause(1000)
+basic.forever(function () {
+    basic.showString("Light: ")
+    basic.showString(SL01.readLight().toString())
+    basic.pause(100)
+
+})
