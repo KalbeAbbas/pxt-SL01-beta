3af243f05ffa633c5ece0de329d79d3f4ff17bc8
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-05 21:25:46 +0500

    Updates.

--- test.ts
+++ test.ts
@@ -1,8 +1,1 @@
-let res = SL01.Begin()
-basic.pause(1000)
-basic.forever(function () {
-    basic.showString("Light: ")
-    basic.showString(SL01.readLight().toString())
-    basic.pause(100)
 
-})
