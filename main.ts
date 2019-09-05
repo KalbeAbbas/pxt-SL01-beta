f24b83fa29ffb03cf7df05527ab2bb25f21c98f2
KalbeAbbas <49709412+KalbeAbbas@users.noreply.github.com>
2019-09-05 17:05:07 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-enum SI1145_ADDR
-{
+enum SI1145_ADDR {
     //% block="0x60"
     ADDR_0x60 = 0x60
 }
 
-namespace SL01
-{
+namespace SL01 {
     let SI1145_I2C_ADDR = SI1145_ADDR.ADDR_0x60
 
     function setreg(reg: number, dat: number): void {
@@ -33,5 +31,37 @@ namespace SL01
     function getInt16LE(reg: number): number {
         pins.i2cWriteNumber(SI1145_I2C_ADDR, reg, NumberFormat.UInt8BE);
         return pins.i2cReadNumber(SI1145_I2C_ADDR, NumberFormat.Int16LE);
+    }
+
+    /* calaulates Ultra Violet Index
+    */
+    //% block="readUVI"
+    //% weight=74 blockGap=8
+    export function readUVI(): number {
+        return getUInt16LE(0x2C)
+    }
+
+    /* calaulates Ambient Light Intensity
+    */
+    //% block="readLight"
+    //% weight=74 blockGap=8
+    export function readLight(): number {
+        return getUInt16LE(0x22)
+    }
+
+    /* calaulates Infra Red Intensity
+    */
+    //% block="readIR"
+    //% weight=74 blockGap=8
+    export function readIR(): number {
+        return getUInt16LE(0x24)
+    }
+
+    /* calaulates Proximity
+    */
+    //% block="readProximity"
+    //% weight=74 blockGap=8
+    export function readProximity(): number {
+        return getUInt16LE(0x26)
     }
 } 
